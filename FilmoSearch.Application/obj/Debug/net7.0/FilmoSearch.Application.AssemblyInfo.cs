6c3b3b3d1b63146aba66d16ff5b4f9f59c7d7dc9
raiver nababkah <ilya.maksimovich.01@gmail.com>
2024-04-17 13:57:01 +0300

    test

--- FilmoSearch.Application/obj/Debug/net7.0/FilmoSearch.Application.AssemblyInfo.cs
+++ FilmoSearch.Application/obj/Debug/net7.0/FilmoSearch.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmoSearch.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b4d491ecc56684c826d152aff2ec9913c55d8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmoSearch.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmoSearch.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FilmoSearch.Domain/obj/Debug/net7.0/FilmoSearch.Domain.AssemblyInfo.cs
+++ FilmoSearch.Domain/obj/Debug/net7.0/FilmoSearch.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmoSearch.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b4d491ecc56684c826d152aff2ec9913c55d8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmoSearch.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmoSearch.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
