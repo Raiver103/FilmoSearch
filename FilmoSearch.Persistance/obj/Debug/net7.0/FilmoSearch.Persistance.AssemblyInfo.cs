eea3efedd259a257ae57da0c0b548aa6fdc7ec00
raiver nababkah <ilya.maksimovich.01@gmail.com>
2024-04-17 13:29:18 +0300

    add migration to identity

--- FilmoSearch.Persistance/obj/Debug/net7.0/FilmoSearch.Persistance.AssemblyInfo.cs
+++ FilmoSearch.Persistance/obj/Debug/net7.0/FilmoSearch.Persistance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmoSearch.Persistance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04d6d5d0b15b935f5bc27e66a4bccb75d435b32a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47127a5f0748b3f12724556238b67941fd6cc1f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmoSearch.Persistance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmoSearch.Persistance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
